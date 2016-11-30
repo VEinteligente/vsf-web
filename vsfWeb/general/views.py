dafe42fa994c4d13ba9135e3b99ae63213013bf9
wmarino13 <wmarino@turpialdev.com>
2016-11-30 15:39:49 -0400

    From view accessed to .json
    .view and .html modified to access .json data and display them in a
    table

--- vsfWeb/general/views.py
+++ vsfWeb/general/views.py
@@ -10,9 +10,12 @@ class AboutUs(TemplateView):
     template_name = "about-us.html"
     
     def get_context_data(self,**kwargs ):
-    	r = requests.get('http://192.168.0.104:8000/events/api/blocked_domains/')
+    	r = requests.get('http://192.168.0.130:8000/events/api/blocked_domains/')
     	context= super(AboutUs,self).get_context_data(**kwargs)
     	context['probando'] = json.loads(r.text)
+    	probando2 = json.loads(r.text)
+    	context['pruebas'] = probando2["results"]
+
     	return context
 
 class Dashboard(TemplateView):

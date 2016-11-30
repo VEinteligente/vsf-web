d17baed9ba0384ca2ede65a65e34492a8dd703cb
wmarino13 <wmarino@turpialdev.com>
2016-11-30 13:33:46 -0400

    Connect with server and read the .json in html
    Server connected, and .json files read in html

--- vsfWeb/general/views.py
+++ vsfWeb/general/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+import requests
 
 # Create your views here.
 from django.views.generic import (TemplateView)
@@ -7,5 +9,11 @@ from django.views.generic import (TemplateView)
 class AboutUs(TemplateView):
     template_name = "about-us.html"
     
+    def get_context_data(self,**kwargs ):
+    	r = requests.get('http://192.168.0.104:8000/events/api/blocked_domains/')
+    	context= super(AboutUs,self).get_context_data(**kwargs)
+    	context['probando'] = json.loads(r.text)
+    	return context
+
 class Dashboard(TemplateView):
     template_name = "dashboard.html"

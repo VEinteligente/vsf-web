21d33425d9e3a450cfba3d21e6620953431983ab
wmarino13 <wmarino@turpialdev.com>
2016-12-01 18:17:24 -0400

    Case List & Case Detail
    Case List added
    Case Detail added (not fully functional)
    Views & Urls added
    Documentation on views.py

--- vsfWeb/general/views.py
+++ vsfWeb/general/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import json
 import requests
+import datetime
 
 # Create your views here.
 from django.views.generic import (TemplateView)
@@ -15,9 +16,10 @@ class BlockedSites(TemplateView):
     def get_context_data(self,**kwargs ):
     	r = requests.get('http://192.168.0.130:8000/events/api/blocked_domains/')
     	context= super(BlockedSites,self).get_context_data(**kwargs)
-    	context['probando'] = json.loads(r.text)
+    	#context test not necessary
+    	context['test'] = json.loads(r.text)
     	probando2 = json.loads(r.text)
-    	context['pruebas'] = probando2["results"]
+    	context['sites'] = probando2["results"]
 
     	return context
 
@@ -27,22 +29,39 @@ class BlockedUrls(TemplateView):
     def get_context_data(self,**kwargs ):
     	r = requests.get('http://192.168.0.130:8000/events/api/blocked_domains/')
     	context= super(BlockedUrls,self).get_context_data(**kwargs)
-    	context['probando'] = json.loads(r.text)
+    	#context test not necessary
+    	context['test'] = json.loads(r.text)
     	probando2 = json.loads(r.text)
-    	context['pruebas'] = probando2["results"]
+    	context['urls'] = probando2["results"]
 
     	return context
 
 class CaseList(TemplateView):
 	template_name = "case-list.html"
 	def get_context_data(self, **kwargs):
-		r = requests.get('http://192.168.0.130:8000/cases/api/detail')
+		r = requests.get('http://192.168.0.130:8000/cases/api/list/')
 	    	context= super(CaseList,self).get_context_data(**kwargs)
-	    	context['probando'] = json.loads(r.text)
+	    	#context test not necessary
+	    	context['test'] = json.loads(r.text)
 	    	probando2 = json.loads(r.text)
-	    	context['pruebas'] = probando2["results"]
+	    	context['cases'] = probando2["results"]
 
 	    	return context
+
+class CaseDetail(TemplateView):
+	template_name = "case-detail.html"
+	def get_context_data(self, **kwargs):
+		r = requests.get('http://192.168.0.130:8000/cases/api/list/')
+	    	context= super(CaseDetail,self).get_context_data(**kwargs)
+	    	#context test not necessary
+	    	context['test'] = json.loads(r.text)
+	    	probando2 = json.loads(r.text)
+	    	context['cases'] = probando2["results"][0]
+	    	aux = datetime.datetime.strptime(probando2["results"][0]["start_date"],'%Y-%m-%dT%H:%M:%SZ')
+	    	context['date'] = aux
+
+	    	return context
+
 
 class Dashboard(TemplateView):
     template_name = "dashboard.html"

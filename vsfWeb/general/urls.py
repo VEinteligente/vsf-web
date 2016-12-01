3be9326d3aa8df5f93b07d27c10c1f144900307f
wmarino13 <wmarino@turpialdev.com>
2016-12-01 10:44:42 -0400

    Organize blocked sites, urls and Case List
    Html's for blocked sites, blocked url and case list added
    Views for BlockedSite, BlockedUrls and CaseList added
    Not functional Caselist

--- vsfWeb/general/urls.py
+++ vsfWeb/general/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 
-from general.views import (Dashboard, AboutUs)
+from general.views import (Dashboard, AboutUs, BlockedSites, BlockedUrls, CaseList)
 
 
 urlpatterns = [
     url(r'^dashboard$',Dashboard.as_view()),
     url(r'^about-us$',AboutUs.as_view()),
+    url(r'^blocked-sites$',BlockedSites.as_view()),
+    url(r'^blocked-urls$',BlockedUrls.as_view()),
+    url(r'^case-list$',CaseList.as_view()),
+
+
+
 
 ] 
--- vsfWeb/general/views.py
+++ vsfWeb/general/views.py
@@ -8,15 +8,42 @@ from django.views.generic import (TemplateView)
 
 class AboutUs(TemplateView):
     template_name = "about-us.html"
+
+class BlockedSites(TemplateView):
+    template_name = "blocked-sites.html"
     
     def get_context_data(self,**kwargs ):
     	r = requests.get('http://192.168.0.130:8000/events/api/blocked_domains/')
-    	context= super(AboutUs,self).get_context_data(**kwargs)
+    	context= super(BlockedSites,self).get_context_data(**kwargs)
     	context['probando'] = json.loads(r.text)
     	probando2 = json.loads(r.text)
     	context['pruebas'] = probando2["results"]
 
     	return context
 
+class BlockedUrls(TemplateView):
+    template_name = "blocked-urls.html"
+    
+    def get_context_data(self,**kwargs ):
+    	r = requests.get('http://192.168.0.130:8000/events/api/blocked_domains/')
+    	context= super(BlockedUrls,self).get_context_data(**kwargs)
+    	context['probando'] = json.loads(r.text)
+    	probando2 = json.loads(r.text)
+    	context['pruebas'] = probando2["results"]
+
+    	return context
+
+class CaseList(TemplateView):
+	template_name = "case-list.html"
+	def get_context_data(self, **kwargs):
+		r = requests.get('http://192.168.0.130:8000/cases/api/detail')
+	    	context= super(CaseList,self).get_context_data(**kwargs)
+	    	context['probando'] = json.loads(r.text)
+	    	probando2 = json.loads(r.text)
+	    	context['pruebas'] = probando2["results"]
+
+	    	return context
+
 class Dashboard(TemplateView):
     template_name = "dashboard.html"
+

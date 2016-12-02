d8c0a57ebbee64306bf42f2079ffcc3863cf4993
wmarino13 <wmarino@turpialdev.com>
2016-12-02 15:33:15 -0400

    Organize project structure & Added Event Information
    -Move into cases the one-case-list.html and case-list.html
    -Added event information on Case Detail
    -Still not functioning the date associated to the event

--- vsfWeb/general/urls.py
+++ vsfWeb/general/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from general.views import (Dashboard, AboutUs, BlockedSites, BlockedUrls, CaseList, CaseDetail)
+from general.views import (Dashboard, AboutUs, BlockedSites, BlockedUrls)
 
 
 urlpatterns = [
@@ -8,10 +8,4 @@ urlpatterns = [
     url(r'^about-us$',AboutUs.as_view()),
     url(r'^blocked-sites$',BlockedSites.as_view()),
     url(r'^blocked-urls$',BlockedUrls.as_view()),
-    url(r'^case-list$',CaseList.as_view()),
-    url(r'^case-detail$',CaseDetail.as_view()),
-
-
-
-
 ] 
--- vsfWeb/general/views.py
+++ vsfWeb/general/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 import json
 import requests
 import datetime
+from django.shortcuts import render
+
 
 # Create your views here.
 from django.views.generic import (TemplateView)
@@ -36,31 +37,7 @@ class BlockedUrls(TemplateView):
 
     	return context
 
-class CaseList(TemplateView):
-	template_name = "case-list.html"
-	def get_context_data(self, **kwargs):
-		r = requests.get('http://192.168.0.130:8000/cases/api/list/')
-	    	context= super(CaseList,self).get_context_data(**kwargs)
-	    	#context test not necessary
-	    	context['test'] = json.loads(r.text)
-	    	probando2 = json.loads(r.text)
-	    	context['cases'] = probando2["results"]
 
-	    	return context
-
-class CaseDetail(TemplateView):
-	template_name = "case-detail.html"
-	def get_context_data(self, **kwargs):
-		r = requests.get('http://192.168.0.130:8000/cases/api/list/')
-	    	context= super(CaseDetail,self).get_context_data(**kwargs)
-	    	#context test not necessary
-	    	context['test'] = json.loads(r.text)
-	    	probando2 = json.loads(r.text)
-	    	context['cases'] = probando2["results"][0]
-	    	aux = datetime.datetime.strptime(probando2["results"][0]["start_date"],'%Y-%m-%dT%H:%M:%SZ')
-	    	context['date'] = aux
-
-	    	return context
 
 
 class Dashboard(TemplateView):

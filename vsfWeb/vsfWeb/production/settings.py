a4470a172ed4de11cdd886b3510a4eedb02ce2e2
AndreaSucreG <andresucre88@gmail.com>
2017-01-22 18:03:40 -0400

    Production configuration
    Production configuration

--- vsfWeb/vsfWeb/production/settings.py
+++ vsfWeb/vsfWeb/production/settings.py
@@ -8,5 +8,5 @@ CONSUMER_SECRET = local_settings.CONSUMER_SECRET
 SERVICES_TOKEN = local_settings.SERVICES_TOKEN
 
 URL_VSF_WEB = 'http://dev.web.pandora.saturno.space'
-URL_VSF = 'http://pandora.saturno.space'
+URL_VSF = 'http://dev.pandora.saturno.space'
 ALLOWED_HOSTS = ['*']

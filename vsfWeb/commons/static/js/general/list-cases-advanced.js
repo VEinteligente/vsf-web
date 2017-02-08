f1e919f618c51c9c446360456b24cfd2d72636c2
wmarino13 <wmarino@turpialdev.com>
2017-02-08 13:08:41 -0400

    <h> headers to hStyle class
    Changed <h#> headers to class="h#Style" on all html and .js where there
    were used

--- vsfWeb/commons/static/js/general/list-cases-advanced.js
+++ vsfWeb/commons/static/js/general/list-cases-advanced.js
@@ -113,7 +113,7 @@ $(document)
 																												$(
 																														".listCasesAdvanced")
 																														.append(
-																																"<div class='col-xs-12 smallBar' ><h5 class='title' style='padding-left: 0px;'>"
+																																"<div class='col-xs-12 smallBar' ><div class='h5Style title' style='padding-left: 0px;'>"
 																																		+ "<div class='col-xs-1 dateStart' style='padding-left: 0px;'></div>"
 																																		+ "<div class='col-xs-1 dateEnd' style='padding-left: 0px;'></div>"
 																																		+ "<div class='col-xs-3 name' style='display: flex; padding-left: 0px;'></div>"
@@ -121,7 +121,7 @@ $(document)
 																																		+ "<div class='col-xs-1 isp' style='padding-left: 0px;'></div>"
 																																		+ "<div class='col-xs-1 category' style='padding-left: 0px; padding-bottom: 5px;'></div>"
 																																		+ "<div class='col-xs-1 region' style='padding-left: 0px;'></div>"
-																																		+ "</h5>     </div>");
+																																		+ "</div>     </div>");
 
 																											}
 
@@ -747,7 +747,7 @@ $("#advanced_search")
 																											$(
 																													".listCasesAdvanced")
 																													.append(
-																															"<div class='col-xs-12 smallBar'><h5 class='title' style='padding-left: 0px;'>"
+																															"<div class='col-xs-12 smallBar'><div class='h5Style title' style='padding-left: 0px;'>"
 																																	+ "<div class='col-xs-1 dateStart' style='padding-left: 0px;'></div>"
 																																	+ "<div class='col-xs-1 dateEnd' style='padding-left: 0px; '></div>"
 																																	+ "<div class='col-xs-3 name' style='display: flex; padding-left: 0px;'></div>"
@@ -755,7 +755,7 @@ $("#advanced_search")
 																																	+ "<div class='col-xs-1 isp' style='padding-left: 0px;'></div>"
 																																	+ "<div class='col-xs-1 category' style='padding-left: 0px; padding-bottom: 5px;'></div>"
 																																	+ "<div class='col-xs-1 region' style='padding-left: 0px;'></div>"
-																																	+ "</h5>     </div>");
+																																	+ "</div>     </div>");
 
 																											// $(
 																											// ".listCasesAdvanced"
@@ -1181,7 +1181,7 @@ function sortDateAsc(parent, child) {
 
 				$(".listCasesAdvanced")
 						.append(
-								"<div class='this col-xs-12 smallBar'><h5 class='title'></h5></div>");
+								"<div class='this col-xs-12 smallBar'><div class='h5Style title'></div></div>");
 				$(".this ").html($(this));
 				$(".this ").removeClass("this ");
 
@@ -1223,7 +1223,7 @@ function sortDateDes(parent, child) {
 
 				$(".listCasesAdvanced")
 						.append(
-								"<div class='this col-xs-12 smallBar'><h5 class='title'></h5></div>");
+								"<div class='this col-xs-12 smallBar'><div class='h5Style title'></div></div>");
 				$(".this ").html($(this));
 				$(".this ").removeClass("this");
 
@@ -1258,7 +1258,7 @@ function sortNameAsc(parent, child) {
 
 				$(".listCasesAdvanced")
 						.append(
-								"<div class='this col-xs-12 smallBar'><h5 class='title'></h5></div>");
+								"<div class='this col-xs-12 smallBar'><div class='h5Style title'></div></div>");
 				$(".this ").html($(this));
 				$(".this ").removeClass("this ");
 
@@ -1290,7 +1290,7 @@ function sortNameDes(parent, child) {
 
 				$(".listCasesAdvanced")
 						.append(
-								"<div class='this col-xs-12 smallBar'><h5 class='title'></h5></div>");
+								"<div class='this col-xs-12 smallBar'><div class='h5Style title'></div></div>");
 				$(".this ").html($(this));
 				$(".this ").removeClass("this");
 

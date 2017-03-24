68cfa13bfa566df0bb4bf2ba21a24af246aab7c1
wmarino13 <wmarino@turpialdev.com>
2017-03-24 17:39:16 -0400

    Measurements Rollback

--- vsfWeb/commons/static/js/summary/mtable.js
+++ vsfWeb/commons/static/js/summary/mtable.js
@@ -76,89 +76,81 @@ $(document).ready(
 															raw_data = JSON
 																	.stringify(value.flags[idcount]);
 
-															if (value.flags==null){
-																country = "null";
-																city ="null";
-																state ="null";
-																isp="null";
-																type="null";
-															}else {$
-																.each(
-																		value.flags,
-																		function(
-																				index,
-																				value) {
+															$
+																	.each(
+																			value.flags,
+																			function(
+																					index,
+																					value) {
 
-																			// country
-																			// of
-																			// the
-																			// measurement
-																			if (value.probe.country == null){
-																				country ="null";
-																			}else{
-																				country = value.probe.country;
+																				// country
+																				// of
+																				// the
+																				// measurement
+																				if (value.probe.country == null){
+																					country ="null";
+																				}else{
+																					country = value.probe.country;
+																					
+																				}
+																				// city
+																				// of
+																				// the
+																				// measurement
 																				
-																			}
-																			// city
-																			// of
-																			// the
-																			// measurement
-																			
-																			if (value.probe.city == null){
-																				city ="null";
-																			}else{
-																				city = value.probe.city;
-																			}
-																			// state
-																			// or
-																			// region
-																			// of
-																			// the
-																			// measurement
-																			if (value.probe.region == null){
-																				state ="null";
-																			}else{
+																				if (value.probe.city == null){
+																					city ="null";
+																				}else{
+																					city = value.probe.city;
+																				}
+																				// state
+																				// or
+																				// region
+																				// of
+																				// the
+																				// measurement
+																				if (value.probe.region == null){
+																					state ="null";
+																				}else{
 
-																				state = value.probe.region;
-																			}
-																			// isp
-																			// of
-																			// the
-																			// measurement
-																			if (value.probe.isp == null){
-																				isp ="null";
-																			}else{
+																					state = value.probe.region;
+																				}
+																				// isp
+																				// of
+																				// the
+																				// measurement
+																				if (value.probe.isp == null){
+																					isp ="null";
+																				}else{
 
-																				isp = value.probe.isp;
-																			}
-																			// type
-																			// of
-																			// measurement
-																			
-																			if (value.type_med == null){
-																				type ="null";
-																			}else{
-																				type = value.type_med;
-																			}
-																			// data
-																			// has
-																			// the
-																			// json
-																			// date
-																			// in
-																			// format
-																			// so
-																			// we
-																			// can
-																			// grab
-																			// day,month
-																			// or
-																			// year
-																			// as
-																			// needed
-																			}
-																		)}
-																			var date = new Date(
+																					isp = value.probe.isp;
+																				}
+																				// type
+																				// of
+																				// measurement
+																				
+																				if (value.type_med == null){
+																					type ="null";
+																				}else{
+																					type = value.type_med;
+																				}
+																				// data
+																				// has
+																				// the
+																				// json
+																				// date
+																				// in
+																				// format
+																				// so
+																				// we
+																				// can
+																				// grab
+																				// day,month
+																				// or
+																				// year
+																				// as
+																				// needed
+																				var date = new Date(
 																						value.date);
 																				// We
 																				// pass
@@ -242,7 +234,4 @@ $(document).ready(
 												+ mid + '</span>');
 							});
 
-					
-					var height_bodyContent = $(".marginPage").height()+50;
-					$(".marginPage").closest(".bodyContent").height(height_bodyContent+ 'px' )
 				});
